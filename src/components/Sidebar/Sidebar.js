057f7f1cea0c2ee058dfae4d6fe34ced6a4e49e5
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-02-01 23:09:44 +0000

    Showing content in the sidebar based on if user is logged in or not

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,35 +1,57 @@
 import { useState } from 'react';
 import { StyledSidebar, ProfileWrapper, ProfileImage, Toggler } from './Sidebar.styles';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { NavigationPaths } from '../../config/routing/NavigationPaths';
 import { useUserContext } from '../../context/userContext';
+import { auth } from '../../config/firebase/firebaseConfig';
+import { Button } from '../Button/Button';
 
 export const Sidebar = () => {
-  const { user, signOut } = useUserContext();
+  const history = useHistory();
+  const { user } = useUserContext();
   const [isOpen, setIsOpen] = useState(false);
   const toggleSidebar = () => {
     setIsOpen((prevState) => !prevState);
   };
   const closeSidebar = () => setIsOpen(false);
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        history.push(NavigationPaths.LOGIN);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <StyledSidebar open={isOpen}>
       <ProfileWrapper>
         <Toggler onClick={toggleSidebar} />
-        <ProfileImage src="https://placeimg.com/300/300/people" alt="<USER> avatar" />
-        <p>{user ? user.email : 'Loading user...'}</p>
+        {user ? (
+          <>
+            <ProfileImage src="https://placeimg.com/300/300/people" alt="<USER> avatar" />
+            <p>{user ? user.email : 'Loading user...'}</p>
+            <ul>
+              <NavLink to={NavigationPaths.TODOS} onClick={closeSidebar}>
+                Todos
+              </NavLink>
 
-        <ul>
-          <NavLink to={NavigationPaths.TODOS} onClick={closeSidebar}>
-            Todos
-          </NavLink>
+              <NavLink to={NavigationPaths.REGISTER} onClick={closeSidebar}>
+                Register
+              </NavLink>
+              <Button secondary onClick={handleSignOut}>
+                Log out
+              </Button>
+            </ul>
+          </>
+        ) : (
           <NavLink to={NavigationPaths.LOGIN} onClick={closeSidebar}>
-            Login
+            <Button>Login</Button>
           </NavLink>
-          <NavLink to={NavigationPaths.REGISTER} onClick={closeSidebar}>
-            Register
-          </NavLink>
-          <button onClick={signOut}>Log out</button>
-        </ul>
+        )}
       </ProfileWrapper>
     </StyledSidebar>
   );
